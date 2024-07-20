9477be25f6eb9c6a1c2d07aec36e27fd30637372
Diana Georgieva <deedee.ag@gmail.com>
2024-07-20 18:44:11 +0300

    Add controlles

---
+++ Web/HappyGift.Web/Controllers/GiftsController.cs
@@ -0,0 +1,23 @@
+﻿namespace HappyGift.Web.Controllers
+{
+    using HappyGift.Data.Models;
+    using HappyGift.Services.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class GiftsController : BaseController
+    {
+        private readonly IGiftsService giftsService;
+
+        public GiftsController(IGiftsService giftsService)
+        {
+            this.giftsService = giftsService;
+        }
+
+        public IActionResult Index()
+        {
+            var gifts = this.giftsService.GetAllAsync<Gift>();
+
+            return this.View(gifts);
+        }
+    }
+}
