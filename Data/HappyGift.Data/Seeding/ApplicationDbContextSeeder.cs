192d08fa5579486170a3a05b9cdebe7c3939dcbe
Diana Georgieva <deedee.ag@gmail.com>
2024-07-20 13:48:23 +0300

    Add Gifts Seeder

--- Data/HappyGift.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Data/HappyGift.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -25,6 +25,7 @@
 
             var seeders = new List<ISeeder>
                           {
+                              new GiftsSeeder(),
                               new RolesSeeder(),
                               new SettingsSeeder(),
                           };
---
+++ Data/HappyGift.Data/Seeding/GiftsSeeder.cs
@@ -0,0 +1,26 @@
+﻿namespace HappyGift.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using HappyGift.Data.Models;
+
+    internal class GiftsSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Gifts.Any())
+            {
+                return;
+            }
+
+            await dbContext.Gifts.AddRangeAsync(
+                new Gift { Name = "E-Reader" },
+                new Gift { Name = "Spa Gift Set" },
+                new Gift { Name = "Chocolate Box" },
+                new Gift { Name = "Designer Wallet" },
+                new Gift { Name = "Bluetooth Speaker" });
+        }
+    }
+}
